2fc152b446eff617d20c2a4e46aed623ff650e9a
lxfly2000 <gaoboyuan8@qq.com>
2016-11-01 00:30:19 +0800

    WTF! I put it in the wrong dir!

--- CutImage/js/main.js
+++ CutImage/js/main.js
@@ -1,85 +1,62 @@
-(function(){
-	var Model=function(config){
+﻿(function(){
+	var Model=function(){
 		var me=this;
-		me.options=config;
-		me.ct=config.ct;
-		var calStyle=document.defaultView.getComputedStyle(me.ct,null);//获得样式对象
-		me.imgWidth=parseInt(calStyle.width);
-		me.imgHeight=parseInt(calStyle.height);
-		me.init();
-	}
-	Model.prototype.init=function(){
-		var me=this;
-		//初始化wrapper
-		me.wrapper=document.createElement("div");
-		me.wrapper.className="wrapper";
-		me.wrapper.style.width=me.options.imgs.length*me.imgWidth+"px";
-		me.ct.appendChild(me.wrapper);
-		
-		me.currentImgNum=0;
-		me.imgs=[];
-		for(var i=0;i<me.options.imgs.length;i++){
-			var img=new Image();
-			img.src=me.options.imgs[i];
-			img.style.width=me.imgWidth+"px";
-			img.style.height=me.imgHeight+"px";
-			img.className="wrapper";
-			me.wrapper.appendChild(img);
-			me.imgs[i]=img;
-		}
-		
-		var circlect=document.createElement("div");
-		circlect.className="circlect";
-		var inner=document.createElement("div");
-		inner.className="inner";
-		circlect.appendChild(inner);
-		me.circles=[];
-		for(var i=0;i<me.options.imgs.length;i++){
-			var span=document.createElement("span");
-			span.className="circle";
-			inner.appendChild(span);
-			me.circles[i]=span;
-		}
-		inner.firstChild.className="circle circle-cur";
-		me.inner=inner;
-		me.ct.appendChild(circlect);
-		
-		me.ct.buttonLeft=document.createElement("button");
-		me.ct.buttonLeft.className="btn btn-left";
-		me.ct.buttonLeft.innerHTML="&lt;";
-		me.ct.appendChild(me.ct.buttonLeft);
-		me.ct.buttonRight=document.createElement("button");
-		me.ct.buttonRight.className="btn btn-right";
-		me.ct.buttonRight.innerHTML="&gt;";
-		me.ct.appendChild(me.ct.buttonRight);
-		
-		me.ct.buttonLeft.addEventListener("click",function(e){
-			me.moveLeft();
-		});
-		me.ct.buttonRight.addEventListener("click",function(e){
-			me.moveRight();
-		});
-	}
-	Model.prototype.moveLeft=function(){
-		var me=this;
-		if(me.currentImgNum==0)return;
-		me.circles[me.currentImgNum].className="circle";
-		me.circles[me.currentImgNum-1].className="circle circle-cur";
-		me.currentImgNum--;
-		for(var i=0;i<me.options.imgs.length;i++){
-			me.imgs[i].style.transform="translate(-"+me.imgWidth*me.currentImgNum+"px,0px)";
-		}
-	}
-	Model.prototype.moveRight=function(){
-		var me=this;
-		if(me.currentImgNum+1==me.options.imgs.length)return;
-		me.circles[me.currentImgNum].className="circle";
-		me.circles[me.currentImgNum+1].className="circle circle-cur";
-		me.currentImgNum++;
-		for(var i=0;i<me.options.imgs.length;i++){
-			me.imgs[i].style.transform="translate(-"+me.imgWidth*me.currentImgNum+"px,0px)";
-		}
+		me.container=document.querySelector("#container");
+		me.image=me.container.querySelector("img");
+		me.scale=1;
+		me.startX=0;
+		me.startY=0;
+		me.initEvent();
 	}
 	
-	window.SlideImg=Model;
+	Model.prototype.bigger=function(){
+		var me=this;
+		me.scale+=0.1;
+		me.updTransform();
+	}
+	
+	Model.prototype.smaller=function(){
+		var me=this;
+		me.scale-=0.1;
+		me.updTransform();
+	}
+	
+	Model.prototype.updTransform=function(){
+		var me=this;
+		me.image.style.transform="scale("+me.scale+","+me.scale+") translate("+me.startX+"px,"+me.startY+"px)";//CSS3属性
+	}
+	
+	Model.prototype.initEvent=function(){
+		var me=this;
+		me.container.addEventListener("mousedown",function(e){
+			me.oldPoint={x:e.clientX,y:e.clientY};
+		});
+		me.container.addEventListener("mouseup",function(e){
+			me.oldPoint=undefined;
+		});
+		me.container.addEventListener("mousemove",function(e){
+			if(me.oldPoint==undefined){
+				return;
+			}
+			me.startX+=e.clientX-me.oldPoint.x;
+			me.startY+=e.clientY-me.oldPoint.y;
+			me.updTransform();
+			me.oldPoint={x:e.clientX,y:e.clientY};
+		});
+	}
+	
+	Model.prototype.crop=function(){
+		var me=this;
+		var x=((me.image.offsetWidth*(me.scale-1)/2+150)/me.scale-me.startX)|0;
+		var y=((me.image.offsetWidth*(me.scale-1)/2+50)/me.scale-me.startY)|0;
+		var w=(300/me.scale)|0;
+		var h=(300/me.scale)|0;
+		
+		var canvas=document.querySelector("canvas");
+		var ctx=canvas.getContext("2d");
+		ctx.clearRect(0,0,300,300);
+		ctx.drawImage(me.image,x,y,w,h,0,0,300,300);
+	}
+	
+	window.ImageEditor=Model;
 }());
--- SlideImg/js/main.js
+++ SlideImg/js/main.js
@@ -16,12 +16,16 @@
 		me.wrapper.style.width=me.options.imgs.length*me.imgWidth+"px";
 		me.ct.appendChild(me.wrapper);
 		
+		me.currentImgNum=0;
+		me.imgs=[];
 		for(var i=0;i<me.options.imgs.length;i++){
 			var img=new Image();
 			img.src=me.options.imgs[i];
 			img.style.width=me.imgWidth+"px";
 			img.style.height=me.imgHeight+"px";
+			img.className="wrapper";
 			me.wrapper.appendChild(img);
+			me.imgs[i]=img;
 		}
 		
 		var circlect=document.createElement("div");
@@ -29,20 +33,52 @@
 		var inner=document.createElement("div");
 		inner.className="inner";
 		circlect.appendChild(inner);
+		me.circles=[];
 		for(var i=0;i<me.options.imgs.length;i++){
 			var span=document.createElement("span");
 			span.className="circle";
 			inner.appendChild(span);
+			me.circles[i]=span;
 		}
 		inner.firstChild.className="circle circle-cur";
 		me.inner=inner;
 		me.ct.appendChild(circlect);
+		
+		me.ct.buttonLeft=document.createElement("button");
+		me.ct.buttonLeft.className="btn btn-left";
+		me.ct.buttonLeft.innerHTML="&lt;";
+		me.ct.appendChild(me.ct.buttonLeft);
+		me.ct.buttonRight=document.createElement("button");
+		me.ct.buttonRight.className="btn btn-right";
+		me.ct.buttonRight.innerHTML="&gt;";
+		me.ct.appendChild(me.ct.buttonRight);
+		
+		me.ct.buttonLeft.addEventListener("click",function(e){
+			me.moveLeft();
+		});
+		me.ct.buttonRight.addEventListener("click",function(e){
+			me.moveRight();
+		});
 	}
 	Model.prototype.moveLeft=function(){
-		
+		var me=this;
+		if(me.currentImgNum==0)return;
+		me.circles[me.currentImgNum].className="circle";
+		me.circles[me.currentImgNum-1].className="circle circle-cur";
+		me.currentImgNum--;
+		for(var i=0;i<me.options.imgs.length;i++){
+			me.imgs[i].style.transform="translate(-"+me.imgWidth*me.currentImgNum+"px,0px)";
+		}
 	}
 	Model.prototype.moveRight=function(){
-		
+		var me=this;
+		if(me.currentImgNum+1==me.options.imgs.length)return;
+		me.circles[me.currentImgNum].className="circle";
+		me.circles[me.currentImgNum+1].className="circle circle-cur";
+		me.currentImgNum++;
+		for(var i=0;i<me.options.imgs.length;i++){
+			me.imgs[i].style.transform="translate(-"+me.imgWidth*me.currentImgNum+"px,0px)";
+		}
 	}
 	
 	window.SlideImg=Model;
